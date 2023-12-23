c5a8e795b2f93f0df1080fb026532f4141fc6cfd
cuu-vnpt <kunnobita97@gmail.com>
2023-12-23 18:12:41 +0700

    new file

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] nameList = {"Nam","Thang","Kien","Binh","Bac","Lam","Huy","GiaLinh","Doquyen"};
+        Console.WriteLine("Nhap ten ban: ");
+        string name = Console.ReadLine();
+        bool isTrue = false;
+        for (int i = 0; i <= nameList.Length-1; i++)
+        {
+            if( nameList[i].Equals(name))
+            {
+                Console.WriteLine($"Co ten trong mang va vi tri {i+1}");
+                isTrue=true;
+                break;
+            }
+        }
+        if(isTrue==false)
+        {
+            Console.WriteLine("Khong co ten");
+        }
+    }
+}
